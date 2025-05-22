eb25a29cbc7fb2b2ae8d54e50ffd1e0077ae68dc
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-05-22 22:58:53 +0530

    Update Sp SQLQuery Image and PDF.sql

--- Sp SQLQuery Image and PDF.sql
+++ Sp SQLQuery Image and PDF.sql
@@ -7,21 +7,22 @@ CREATE TABLE [dbo].[Teacher](
 	ALTER TABLE Teacher ADD PdfPath VARCHAR(255);
 
 
-	UPDATE Teacher SET ImagePath = '~/Images/1_Std.jpg' WHERE Id = 501;
+UPDATE Teacher SET ImagePath = '~/Images/1_Std.jpg' WHERE Id = 501;
 UPDATE Teacher SET ImagePath = '~/Images/2_Std.jpg' WHERE Id = 502;
 UPDATE Teacher SET ImagePath = '~/Images/3_Std.jpeg' WHERE Id = 503;
 UPDATE Teacher SET ImagePath = '~/Images/4_Std.jpg' WHERE Id = 504;
 UPDATE Teacher SET ImagePath = '~/Images/5_Std.jpeg' WHERE Id = 505;
 UPDATE Teacher SET ImagePath = '~/Images/6_Std.jpg' WHERE Id = 506;
 
-UPDATE Teacher SET PdfPath = '"~\PDFs\1.pdf"' WHERE Id = 501;
-UPDATE Teacher SET PdfPath = '"~\PDFs\2.pdf"' WHERE Id = 502;
-UPDATE Teacher SET PdfPath = '"~\PDFs\3.pdf"' WHERE Id = 503;
-UPDATE Teacher SET PdfPath = '"~\PDFs\4.pdf"' WHERE Id = 504;
-UPDATE Teacher SET PdfPath = '"~\PDFs\5.pdf"' WHERE Id = 505;
-UPDATE Teacher SET PdfPath = '"~\PDFs\6.pdf"' WHERE Id = 506;
-UPDATE Teacher SET PdfPath = '"~\PDFs\7.pdf"' WHERE Id = 507;
-UPDATE Teacher SET PdfPath = '"~\PDFs\8.pdf"' WHERE Id = 508;
+UPDATE Teacher SET PdfPath = '~/PDFs/1.pdf' WHERE Id = 501;
+UPDATE Teacher SET PdfPath = '~/PDFs/2.pdf' WHERE Id = 502;
+UPDATE Teacher SET PdfPath = '~/PDFs/3.pdf' WHERE Id = 503;
+UPDATE Teacher SET PdfPath = '~/PDFs/4.pdf' WHERE Id = 504;
+UPDATE Teacher SET PdfPath = '~/PDFs/5.pdf' WHERE Id = 505;
+UPDATE Teacher SET PdfPath = '~/PDFs/6.pdf' WHERE Id = 506;
+UPDATE Teacher SET PdfPath = '~/PDFs/7.pdf' WHERE Id = 507;
+UPDATE Teacher SET PdfPath = '~/PDFs/8.pdf' WHERE Id = 508;
+
 
 -- Display All Details
 [dbo].[AllTeachersDetail_SP]
